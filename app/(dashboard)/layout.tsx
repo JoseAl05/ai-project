3fa0a5b26ed62d73bad0b65cd0d2efef75057d41
JoseAl05 <jparancibialinker@gmail.com>
2023-08-15 02:09:24 -0400

    added variables to handle pro subscription state

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,20 +1,29 @@
 import Navbar from '@/components/navbar/Navbar';
 import Sidebar from '@/components/sidebar/Sidebar';
+import { apiLimitCount } from '@/lib/apiLimit';
+import { checkSubscription } from '@/lib/subscription';
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
     children
 }: {
     children: React.ReactNode
 }) => {
+
+    const apiCounter = await apiLimitCount();
+    const isPro = await checkSubscription();
+
     return (
         <div className='h-full relative'>
-            <div className='hidden h-full z-[80] bg-gray-900 md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 '>
+            <div className='hidden h-full bg-gray-900 md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 '>
                 <div>
-                    <Sidebar />
+                    <Sidebar
+                        apiLimitCount={apiCounter}
+                        isPro={isPro}
+                    />
                 </div>
             </div>
             <main className='md:pl-72'>
-                <Navbar/>
+                <Navbar />
                 {children}
             </main>
         </div>
