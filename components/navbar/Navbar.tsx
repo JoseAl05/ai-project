f9ff348c0e40cdca2ba68318a6d5840967df10bd
JoseAl05 <jparancibialinker@gmail.com>
2023-08-15 02:13:31 -0400

    passed variables to mobile sidebar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,12 +1,21 @@
 
+import { apiLimitCount } from '@/lib/apiLimit';
 import MobileSidebar from '../sidebar/MobileSidebar';
 import { Button } from '../ui/button';
 import { UserButton } from '@clerk/nextjs';
+import { checkSubscription } from '@/lib/subscription';
 
-const Navbar = () => {
+const Navbar = async() => {
+
+    const apiCounter = await apiLimitCount();
+    const isPro = await checkSubscription();
+
     return (
         <div className='flex items-center p-4'>
-            <MobileSidebar />
+            <MobileSidebar
+                apiLimitCount={apiCounter}
+                isPro={isPro}
+            />
             <div className='flex justify-end w-full'>
                 <UserButton afterSignOutUrl='/' />
             </div>
